a9ebf4ad85432ceeb7beb56374e7319600905726
Tim <tim@timschroeder.net>
2016-06-25 17:10:15 +0200

    Initial Commit

---
+++ Brow/Brow.h
@@ -0,0 +1,15 @@
+//
+//  Brow.h
+//  Brow
+//
+//  Created by Tim Schröder on 25.06.16.
+//  Copyright © 2016 Tim Schröder. All rights reserved.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+@interface Brow : NSPreferencePane
+
+- (void)mainViewDidLoad;
+
+@end
---
+++ Brow/Brow.m
@@ -0,0 +1,17 @@
+//
+//  Brow.m
+//  Brow
+//
+//  Created by Tim Schröder on 25.06.16.
+//  Copyright © 2016 Tim Schröder. All rights reserved.
+//
+
+#import "Brow.h"
+
+@implementation Brow
+
+- (void)mainViewDidLoad
+{
+}
+
+@end
